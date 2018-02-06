089529390495dd6118d2c1144c934ab9e863de55
jelic98 <jelic.ecloga@gmail.com>
2018-02-06 03:03:06 +0100

    [BUG FIX] Negative scroll

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 $(document).scroll(function() { 
-	if($(document).scrollTop() >= 0) {
+	if($(document).scrollTop() > 0) {
 		if($(document).scrollTop() >= $("header").height() / 2) {
 	   		$("header").addClass("scrolling");
 	   		$(".status").fadeOut();
