087d42bf566505f5c6b961df7d42da7e23e11317
jelic98 <jelic.ecloga@gmail.com>
2018-02-07 18:03:50 +0100

    Added css shake offline

--- logic/connect.php
+++ logic/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$connect = mysqli_connect("localhost", "root", "root"); 
-    mysqli_select_db($connect, "soxbty");  
+	$connect = mysqli_connect("localhost", "root", "root");
+	mysqli_select_db($connect, "soxbty");
 ?>
--- ui/link.php
+++ ui/link.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../css/main.css">
 <link rel="stylesheet" href="../css/header.css">
 <link rel="stylesheet" href="../css/footer.css">
-<link rel="stylesheet" type="text/css" href="http://csshake.surge.sh/csshake.min.css">
+<link rel="stylesheet" href="../css/shake.css">
 <link rel="apple-touch-icon" sizes="180x180" href="../img/icon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="../img/icon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../img/icon/favicon-16x16.png">
