faefd7eeafe6d701936b207e2564d4fba304d78c
jelic98 <jelic.ecloga@gmail.com>
2018-04-29 20:17:48 +0200

    Removed dummy gallery photos

--- actions/gallery_load.php
+++ actions/gallery_load.php
@@ -8,24 +8,12 @@
 	$offset = strip($_GET['o']);
 	$limit = strip($_GET['l']);
 
-	// TODO uncomment instagram and remove dummy images
-	/*$images = instagram_images();
+	$images = instagram_images();
 
 	foreach($images as $key => $value) {
 		echo '<div class="item" data-sr>';
 		echo '<a href="' . $key . '">';
 		echo '<img class="item-image" src="' . $value . '" />';
-		echo '<div class="item-overlay">';
-		echo '<div class="tint hover"></div>';
-		echo '</div>';
-		echo '</a>';
-		echo '</div>';
-	}*/
-					
-	for($i = 0; $i < 8; $i++) {
-		echo '<div class="item" data-sr>';
-		echo '<a href="../img/random.jpg">';
-		echo '<img class="item-image" src="../img/random.jpg" />';
 		echo '<div class="item-overlay">';
 		echo '<div class="tint hover"></div>';
 		echo '</div>';
